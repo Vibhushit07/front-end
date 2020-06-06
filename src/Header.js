cb1ceb902e60faee10364b389d209ed92bb9386f
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-06-06 20:42:11 +0530

    Updated header

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,31 @@ import React, { Component } from 'react';
 
 class Header extends Component {
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: null
+        }
+    }
+
+    submit() {
+        this.setState({name: document.getElementById("name").value});
+    }
+
     render(){
         return(
-            <div>
-                <input type = "text" placeholder = "Enter user name" /> 
-                <input type = "button" placeholder = "Search" value = "Search" />
+            <div id = "header">
+                <input type = "text" placeholder = "Enter user name" id = "name" /> 
+                <input type = "button" placeholder = "Search" value = "Search" onClick = {() => this.submit()} />
+                {
+                    this.state.name === null ?
+                        <div> </div>
+                    :
+                        <div>
+                            <p>Name: {this.state.name}</p>
+                        </div>
+                }
             </div>
         )
     }
